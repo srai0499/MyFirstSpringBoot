b36f3430e7c03cc2703425659fbcaa7be27b56ed
Sejal <srai5@atlassian.com>
2023-05-29 00:40:51 +0530

    spring boot

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Spring!";
+    @GetMapping("/greet")
+    public String greet() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/custom")
+    public String customMessage() {
+        return "This is a custom message.";
     }
 }
